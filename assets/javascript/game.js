441ce40aba7d00eed35c54cd3b5136f5e79fe078
Adam Miclot <amiclot09@gmail.com>
2017-08-02 10:29:10 -0400

    Final Psychic Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,26 +4,34 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var trackUserGuesses = [];	
+var computerGuess = possibilities[Math.floor(Math.random() * possibilities.length)];
 
-
-
-
-document.onkeyup = function(event) {
-
-//capture user guess change to lowercase to match possibilities
-var userGuess = event.key.toLowerCase();
-console.log("user guess = " + userGuess);
+var winsEl = document.getElementById('wins');
+var lossesEl = document.getElementById('losses');
+var userGuessEl = document.getElementById('userGuess');
+var trackUserGuessesE1 = document.getElementById('trackUserGuesses');
+var guessesLeftEl = document.getElementById('guessesLeft');
 
 //set computer guess (this changes every upkeep, when moved outside function, it is nolonger being check against logic statements)
-var computerGuess = possibilities[Math.floor(Math.random() * possibilities.length)];
-console.log("computer guess = " + computerGuess);
+
+var reset = function(){
+	computerGuess = possibilities[Math.floor(Math.random() * possibilities.length)];
+	console.log("computer guess = " + computerGuess);
+	trackUserGuesses = [];
+}
+
+
+
+var startGame = function(userGuess, computerGuess){
+	console.log("computer guess = " + computerGuess);
 
 //if userguess matches
 if (userGuess == computerGuess) {
 	guessesLeft = 9;
 	wins++;
 	console.log("wins = " + wins);
-	trackUserGuesses = [];
+	reset();
+
 }
 
 //if user guess doesn't match and the user still has guesses left
@@ -44,9 +52,22 @@ else if (guessesLeft === 0){
 	guessesLeft = 9;
 	losses++;
 	console.log("loses = " + losses);
-	trackUserGuesses = [];
-
+	reset();
 }
 
 
-	}
+}
+
+document.onkeyup = function(event) {
+//capture user guess change to lowercase to match possibilities
+	var userGuess = event.key.toLowerCase();
+	console.log("user guess = " + userGuess);
+	//starts game
+	startGame(userGuess, computerGuess);
+	trackUserGuessesE1.textContent = trackUserGuesses;
+    winsEl.textContent = wins;
+    lossesEl.textContent = losses;
+    guessesLeftEl.textContent = guessesLeft;
+    userGuessEl.textContent = userGuess;
+}
+
